461de52210bdaa9518c62a118e0bda72fa44c5fa
Judirale13 <93725600+Judirale13@users.noreply.github.com>
2023-08-05 20:32:55 -0500

    feat: :art: Finish myApp.js config

--- myApp.js
+++ myApp.js
@@ -1,8 +1,24 @@
-let express = require('express');
+const express = require('express');
 let app = express();
-console.log("Hello World");
-
-
+app.get("/", function (req, res) {
+    absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+    res.json({ "message": "Hello json" })
+});
+app.get("/json", function (req, res) {
+    const messageJson = "Hello json";
+    let message;
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message = messageJson.toUpperCase();
+    } else {
+        message = messageJson;
+    }
+    const data = {
+        "message": message
+    };
+    res.json(data);
+});
+app.use("/public", express.static(__dirname + '/public'));
 
 
 
